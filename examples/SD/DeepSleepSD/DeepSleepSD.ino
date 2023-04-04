61579d9607b633ca42afd5cdcd4c68720e54f0e7
paul <strid3r21@gmail.com>
2023-04-04 14:09:26 -0400

    minor update

--- examples/SD/DeepSleepSD/DeepSleepSD.ino
+++ examples/SD/DeepSleepSD/DeepSleepSD.ino
@@ -1,39 +1,167 @@
-#include <BDL.h> //include the bee motion s3 helper library
 
-BDL bdl;  //pull the bdl class from the helper and name it bdl so we can use it below;
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+#include <BDL.h>
+#include "RTClib.h"
 
-void setup() {
-  
-  bdl.begin(); // Initalize the helper library. if you fail to do this the program will not work as intended.
+RTC_DS3231 rtc;
 
-  // Brightness is 0-255. We set it to 1/2 brightness here
-  bdl.setPixelBrightness(255 / 2);
+BDL bdl;
+
+
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  30        /* Time ESP32 will be asleep for (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+String dataMessage;
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
 }
 
-int color = 0;
+
+// Initialize SD card
+void initSDCard(){
+   if (!SD.begin()) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+}
+
+// Write to the SD card
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+// Append data to the SD card
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void setup() {
+  Serial.begin(115200);
+  bdl.begin();
+
+   if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  DateTime now = rtc.now();
+  
+  initSDCard();
+ 
+  
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doesn't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Date, Day, Time, RTC Temp, Battery Voltage, USB Connected \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  float tempF = (rtc.getTemperature()*1.8)+32;  // get the temperature from the RTC and convert it to farenheit. for Celcius just use rtc.getTemperature() by itself.
+  float battery = bdl.getBatteryVoltage(); // get the battery voltage so we can monitor the battery health.
+  bool usb = bdl.getVbusPresent();
+
+  String year = String(now.year(), DEC);
+  String month = String(now.month(), DEC);
+  String day = String(now.day(), DEC);
+  String hour = String(now.hour(), DEC);
+  String minute = String(now.minute(), DEC);
+  String second = String(now.second(), DEC);
+
+    //Concatenate all info separated by commas
+    dataMessage =  String(month +"/" + day +"/" + year) + "," + String(daysOfTheWeek[now.dayOfTheWeek()]) + "," + String(hour + ":" + minute + ":" + second ) + ","  + String(tempF) + "," + String(battery) +  "," + String(usb) + "\r\n";
+    Serial.print("Saving data: ");
+    Serial.println(dataMessage);
+
+    //Append the data to file
+    appendFile(SD, "/data.txt", dataMessage.c_str());
+    delay(5000);
+
+    Serial.flush(); 
+  esp_deep_sleep_start();
+}
 
 void loop() {
-  // colorWheel cycles red, orange, ..., back to red at 256
-  bdl.setPixelColor(BDL::colorWheel(color));
-  color++;
-
-  // or you can select a specific color like so
-  // the predefined colors you can choose from are green, red, blue, pink, purple, yellow, white, aqua, orange and off.
-
-  // bdl.setPixelColor(blue);  
-  // uncomment the line above to use it.
-
-  // or by using RGB color values.
-
-  // bdl.setPixelColor(150,0,150).
-  // uncomment the line above to use it.
-  // find a list of all color RGB values here https://www.rapidtables.com/web/color/RGB_Color.html
+ 
   
-  // or you can use any color hex code.
-  
-  // bdl.setPixelColor(0x00FF0000).
-  // uncomment the line above to use it.
-  // find hex color codes here https://soft.infomir.com/stb/522/Doc/ColorCodes.html
-
-  delay(15);
 }
